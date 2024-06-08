9a9cfd97b2d95f6aa04c7ca234d8deab5b61a8cf
ruranjo <rubens.rangel.job@gmail.com>
2024-06-07 19:35:10 -0500

    Rubens make modifications

--- src/components/NavbarAdmin/NavbarAdmin.tsx
+++ src/components/NavbarAdmin/NavbarAdmin.tsx
@@ -20,14 +20,7 @@ const Navbar: React.FC<Props> = ({ isAdmin, title }) => {
     <div className={`${isAdmin ? "bg-secondary" : "bg-blue-600" } p-5 rounded-lg mb-5 text-white flex flex-col sm:flex-row items-center justify-between border-2 border-blue-200`}>
       <div className="font-bold uppercase mb-3 sm:mb-0">{title}</div>
       <div className="flex flex-col sm:flex-row items-center gap-3 sm:gap-5 w-full sm:w-auto">
-        <div className="flex items-center gap-3 bg-white text-gray-600 p-2 rounded-lg w-full sm:w-auto">
-          <MdSearch />
-          <input
-            type="text"
-            placeholder="Search..."
-            className="bg-transparent border-none text-black w-full sm:w-auto"
-          />
-        </div>
+        
         <div className="flex gap-3 sm:gap-5 mt-3 sm:mt-0">
           <MdOutlineChat
             size={20}
